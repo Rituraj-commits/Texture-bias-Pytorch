30e34b2216e270ece297d79698731e4890a9981e
abdurrfayjie <fayjie92@gmail.com>
2021-07-18 22:28:02 +0200

    Time_distributed_layer_added

--- texture-bias/train_DOGLSTM.py
+++ texture-bias/train_DOGLSTM.py
@@ -1,6 +1,14 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+#! /usr/bin/env python
+""" 
+script for training DOGLSTM 
+"""
+__authors__ = "Rituraj Dutta, Abdur R. Fayjie"
+__emails__ = "fayjie92@gmail.com"
+
+
+#from __future__ import absolute_import
+#from __future__ import division
+#from __future__ import print_function
 import os
 #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import matplotlib.pyplot as plt
@@ -19,6 +27,7 @@ t_l_path   = 'fss_test_set.txt'
 Best_performance = 0.00
 Valid_miou = []
 
+# Parameter configuration
 GPU = options.gpu                      ## default is gpu:0
 CUDA = options.use_cuda                ## default is True
 LEARNING_RATE = options.learning_rate  ## default is 0.0001
@@ -28,10 +37,10 @@ EPOCHS = options.epochs                ## default is 50
 Train_list, Test_list = U.Get_tr_te_lists(options, t_l_path)
 
 ## DOG parameters
-
 kernet_shapes = [3, 5, 7, 9]
 k_value = np.power(2, 1/3)
 sigma   = 1.6
+
 
 class Lambda(nn.Module):
     def __init__(self, lambd):
@@ -49,6 +58,7 @@ def get_kernel_gussian(kernel_size, Sigma=1, in_channels = 320):
     kernel_weights = np.expand_dims(kernel_weights, axis=-1)
     return kernel_weights
 
+
 def GlobalAveragePooling2D_r(f):
     def func(x):
         repc = int(x.shape[1])
@@ -62,49 +72,106 @@ def GlobalAveragePooling2D_r(f):
         return x
     return Lambda(func)  ##
 
+
 def Rep_mask(f):
     def func(x):
         x = torch.repeat_interleave(x,f,dim=1)
         return x
     return Lambda(func)  ##
 
+
 def common_representation(x1,x2):
 ### Not Implemented #####
     return
+
+
+### time distributed layer
+# Time distribution is in sequencial order 
+class TimeDistributed(nn.Module):
+    """
+    Applies a module over tdim identically for each step.
+    + Low memory condition.
+    """
+    def __init__(self, module, low_mem=False, tdim=1):
+        super(TimeDistributed, self).__init__()
+        self.module = module
+        self.low_mem = low_mem
+        self.tdim = tdim
+        
+
+    def forward(self, *args, **kwargs):
+        """
+        Args: 
+            - Input X : [bs, seq_len , C, W, H]
+        Return:
+            - Output X : [bs, seq_len , C, W, H]
+        """
+        if self.low_mem or self.tdim!=1: 
+            return self.low_mem_forward(*args)
+        else:
+            #only support tdim=1
+            inp_shape = args[0].shape
+            bs, seq_len = inp_shape[0], inp_shape[1]   
+            out = self.module(*[x.view(bs*seq_len, *x.shape[2:]) for x in args], **kwargs)
+            out_shape = out.shape
+            return out.view(bs, seq_len,*out_shape[1:])
+    
+
+    def low_mem_forward(self, *args, **kwargs):                                           
+         """
+        Args: 
+            - Input X : [bs, seq_len , C, W, H]
+        Return:
+            - Output X : [bs, seq_len , C, W, H]
+        """
+        tlen = args[0].shape[self.tdim]
+        args_split = [torch.unbind(x, dim=self.tdim) for x in args]
+        out = []
+        for i in range(tlen):
+            out.append(self.module(*[args[i] for args in args_split]), **kwargs)
+        return torch.stack(out,dim=self.tdim)
+
+
+    def __repr__(self):
+        """
+        Return:
+            - Time distribution over the input module
+        """
+        return f'TimeDistributed({self.module})'
 
 
 class Texture:
     def __init__(self,s_encoded,q_encoded,s_mask):
         super(Texture,self).__init__()
 
-        self.Sigma1_kernel = get_kernel_gussian(kernel_size = kernet_shapes[0], Sigma = sigma*np.power(k_value, 1), in_channels = 320)
-        self.Sigma2_kernel = get_kernel_gussian(kernel_size = kernet_shapes[1], Sigma = sigma*np.power(k_value, 2), in_channels = 320)    
-        self.Sigma3_kernel = get_kernel_gussian(kernel_size = kernet_shapes[2], Sigma = sigma*np.power(k_value, 3), in_channels = 320)     
-        self.Sigma4_kernel = get_kernel_gussian(kernel_size = kernet_shapes[3], Sigma = sigma*np.power(k_value, 4), in_channels = 320)    
+        self.Sigma1_kernel = get_kernel_gussian(kernel_size = kernet_shapes[0],\
+        Sigma = sigma*np.power(k_value, 1), in_channels = 320)
+        self.Sigma2_kernel = get_kernel_gussian(kernel_size = kernet_shapes[1],\
+        Sigma = sigma*np.power(k_value, 2), in_channels = 320)    
+        self.Sigma3_kernel = get_kernel_gussian(kernel_size = kernet_shapes[2],\
+        Sigma = sigma*np.power(k_value, 3), in_channels = 320)     
+        self.Sigma4_kernel = get_kernel_gussian(kernel_size = kernet_shapes[3],\
+        Sigma = sigma*np.power(k_value, 4), in_channels = 320)    
 
         ## Depthwise Convolution
-
-        self.Sigma1_layer = nn.Conv2d(in_channels=320,out_channels=320,kernel_size=kernet_shapes[0],groups=320,padding='same',bias=False)
-        self.Sigma2_layer = nn.Conv2d(in_channels=320,out_channels=320,kernel_size=kernet_shapes[1],groups=320,padding='same',bias=False)
-        self.Sigma3_layer = nn.Conv2d(in_channels=320,out_channels=320,kernel_size=kernet_shapes[2],groups=320,padding='same',bias=False)
-        self.Sigma4_layer = nn.Conv2d(in_channels=320,out_channels=320,kernel_size=kernet_shapes[3],groups=320,padding='same',bias=False)
+        self.Sigma1_layer = nn.Conv2d(in_channels=320,out_channels=320,\
+        kernel_size=kernet_shapes[0],groups=320,padding='same',bias=False)
+        self.Sigma2_layer = nn.Conv2d(in_channels=320,out_channels=320,\
+        kernel_size=kernet_shapes[1],groups=320,padding='same',bias=False)
+        self.Sigma3_layer = nn.Conv2d(in_channels=320,out_channels=320,\
+        kernel_size=kernet_shapes[2],groups=320,padding='same',bias=False)
+        self.Sigma4_layer = nn.Conv2d(in_channels=320,out_channels=320,\
+        kernel_size=kernet_shapes[3],groups=320,padding='same',bias=False)
 
         self.GlobalAveragePooling = GlobalAveragePooling2D_r(s_mask)
 
         ### Not Implemented #####
 
 
-
-
-
-
-
-
-
 def train():
-
-    
-    
+    """
+    training part for the model
+    """    
     encoder = get_encoder('efficientnet-b1',in_channels=3,weights=None)
 
     encoder.cuda(GPU)
@@ -122,13 +189,12 @@ def train():
 
           
 
-   
-
-
-
-if __name__=="main":
-
-    train()
+if __name__=="__main__":
+    # train()
+    print('--- Variable Tensor Time Distribution Layer ---\n')
+    tdconv = TimeDistributed(nn.Conv2d(2, 5, 3, 1, 1), tdim=1)
+    x = tdconv(torch.rand(3, 10, 2, 8, 8))
+    print(x.shape)
     
             
          
